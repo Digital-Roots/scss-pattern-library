01fc6337e7356a9f2835076d763e40594792d4d7
Robert Edwardes <rdedwardes@gmail.com>
2017-10-10 07:14:43 -0400

    js work
    JS for 00-elements/typeography

--- js/main.js
+++ js/main.js
@@ -1,15 +1,74 @@
 $(document).ready(function(){
   $("#unbuntu").click(function(){
     $("span.name").empty("span.name");
-    $("span.class").empty("code.class");
+    $("code.class").empty("code.class");
     if($('div#type-face').attr('class') == ''){
       $("div#type-face").addClass("unbuntu");
-      console.log('it added');
     }else{
-      $("div#type-face").toggleClass("unbuntu");
-      console.log('it toggled');
+      $("div#type-face").removeAttr('class');
+      $("div#type-face").addClass("unbuntu");
     }
     $("span.name").append("unbuntu");
     $("code.class").append('class= ubuntu');
   });
+  $("#Comfortaa").click(function(){
+    $("span.name").empty("span.name");
+    $("code.class").empty("code.class");
+    if($('div#type-face').attr('class') == ''){
+      $("div#type-face").addClass("Comfortaa");
+    }else{
+      $("div#type-face").removeAttr('class');
+      $("div#type-face").addClass("Comfortaa");
+    }
+    $("span.name").append("Comfortaa");
+    $("code.class").append('class= Comfortaa');
+  });
+  $("#BRUX").click(function(){
+    $("span.name").empty("span.name");
+    $("code.class").empty("code.class");
+    if($('div#type-face').attr('class') == ''){
+      $("div#type-face").addClass("BRUX");
+    }else{
+      $("div#type-face").removeAttr('class');
+      $("div#type-face").addClass("BRUX");
+    }
+    $("span.name").append("BRUX");
+    $("code.class").append('class= BRUX');
+  });
+  $("#Hamurz").click(function(){
+    $("span.name").empty("span.name");
+    $("code.class").empty("code.class");
+    if($('div#type-face').attr('class') == ''){
+      $("div#type-face").addClass("Hamurz");
+    }else{
+      $("div#type-face").removeAttr('class');
+      $("div#type-face").addClass("Hamurz");
+    }
+    $("span.name").append("Hamurz");
+    $("code.class").append('class= Hamurz');
+  });
+  $("#kust").click(function(){
+    $("span.name").empty("span.name");
+    $("code.class").empty("code.class");
+    if($('div#type-face').attr('class') == ''){
+      $("div#type-face").addClass("kust");
+    }else{
+      $("div#type-face").removeAttr('class');
+      $("div#type-face").addClass("kust");
+    }
+    $("span.name").append("kust");
+    $("code.class").append('class= kust');
+  });
+  // $("#Comfortaa").click(function(){
+  //   $("span.name").empty("span.name");
+  //   $("code.class").empty("code.class");
+  //   if($('div#type-face').attr('class') == ''){
+  //     $("div#type-face").addClass("Comfortaa");
+  //   }else{
+  //     $("div#type-face").removeAttr('class');
+  //     $("div#type-face").addClass("Comfortaa");
+  //   }
+  //   $("span.name").append("Comfortaa");
+  //   $("code.class").append('class= Comfortaa');
+  // });
 });
